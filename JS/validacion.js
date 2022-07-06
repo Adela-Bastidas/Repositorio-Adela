3c6adc4562af4287335c2e09d8218a3382d32545
Adela <mayerlinbastidas92@gmail.com>
2022-07-05 19:26:49 -0500

    Agregando cambios en index y validación

--- JS/validacion.js
+++ JS/validacion.js
@@ -1,4 +1,23 @@
-var nombre = document.querySelector('#nombre');
-var email = document.querySelector('#email');
-var asunto = document.querySelector('#asunto');
-var mensaje = document.querySelector('#mensajes');
+var nombre = document.getElementById('nombre');
+var asunto = document.getElementById('asunto');
+var mensaje = document.getElementById('mensajes');
+var email = document.getElementById('email');
+var formulario = document.getElementById('formulario');
+const botonGuardar = document.getElementById('enviar');
+var url = 'https://formsubmit.co/mayerlinbastidas92@gmail.com';
+
+botonGuardar.addEventListener('click', () => {
+
+	if (nombre.value.length === 0 || 
+		asunto.value.length === 0 || 
+		mensaje.value.length === 0 || 
+		email.value.length === 0) {
+		alert('¡Algún campo está vacío!');
+		formulario.action = window.location.roload();
+		formulario.submit();
+	}else{
+		formulario.action = url;
+		formulario.submit();
+	}
+	 
+});
